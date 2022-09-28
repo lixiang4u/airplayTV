2121776cb4a99260da771072e479c6fe599a5ade
example <username@example.com>
2022-09-28 17:53:03 +0800

    添加播放

--- service/LVMovie.go
+++ service/LVMovie.go
@@ -31,7 +31,7 @@ func (x LVMovie) Detail(id string) model.MovieInfo {
 }
 
 func (x LVMovie) Source(sid, vid string) model.Video {
-	return model.Video{}
+	return x.lvVideoSource(sid, vid)
 }
 
 // ===
@@ -102,7 +102,7 @@ func (x LVMovie) lvVideoDetail(id string) model.MovieInfo {
 				continue
 			}
 			info.Links = append(info.Links, model.Link{
-				Id:    fmt.Sprintf("%s-%s-%s", id, urls[1], urls[2]),
+				Id:    fmt.Sprintf("%s-%s", urls[1], urls[2]),
 				Name:  fmt.Sprintf("资源%s", urls[2]),
 				Url:   urls[3],
 				Group: fmt.Sprintf("group_%s", urls[1]),
@@ -115,6 +115,49 @@ func (x LVMovie) lvVideoDetail(id string) model.MovieInfo {
 	})
 
 	err := c.Visit(fmt.Sprintf(lvDetailUrl, id))
+	if err != nil {
+		log.Println("[visit.error]", err.Error())
+	}
+
+	return info
+}
+
+func (x LVMovie) lvVideoSource(sid, vid string) model.Video {
+	var info = model.Video{}
+	info.Id = vid
+	info.Source = fmt.Sprintf(lvDetailUrl, vid)
+
+	c := x.Movie.NewColly()
+
+	c.OnHTML(".stui-player__detail", func(element *colly.HTMLElement) {
+		info.Name = strings.TrimSpace(element.ChildText(".title"))
+	})
+	c.OnResponse(func(response *colly.Response) {
+		tmpList := strings.Split(sid, "-")
+		log.Println("[tmpList]", util.ToJSON(tmpList, true))
+		if len(tmpList) != 2 {
+			return
+		}
+		// urls[1][0]='https://v10.dious.cc/20210707/jxnrMrb8/index.m3u8';
+		reg := regexp.MustCompile(`urls\[(\d+)\]\[(\d+)\]='(\S+)'`)
+		urlList := reg.FindAllStringSubmatch(string(response.Body), -1)
+		log.Println("[urlList]", util.ToJSON(urlList, true))
+		for _, urls := range urlList {
+			if len(urls) != 4 {
+				continue
+			}
+			if urls[1] == tmpList[0] && urls[2] == tmpList[1] {
+				info.Url = urls[3]
+				info.Type = "hls"
+			}
+		}
+	})
+
+	c.OnRequest(func(request *colly.Request) {
+		log.Println("Visiting", request.URL.String())
+	})
+
+	err := c.Visit(fmt.Sprintf(lvDetailUrl, vid))
 	if err != nil {
 		log.Println("[visit.error]", err.Error())
 	}
