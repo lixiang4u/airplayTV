3673508f67f7857cbde6bf36997e7d6e5b256931
example <username@example.com>
2022-09-28 17:36:00 +0800

    添加详情接口

--- service/LVMovie.go
+++ service/LVMovie.go
@@ -6,11 +6,13 @@ import (
 	"github.com/lixiang4u/airplayTV/model"
 	"github.com/lixiang4u/airplayTV/util"
 	"log"
+	"regexp"
 	"strings"
 )
 
 var (
-	lvSearchUrl = "https://lvv2.com/mv/search/q-%s/type-/page-%d" //https://lvv2.com/mv/search/q-天/type-/page-2
+	lvSearchUrl = "https://lvv2.com/mv/search/q-%s/type-/page-%d"
+	lvDetailUrl = "https://lvv2.com/mv/play/id-%s"
 )
 
 type LVMovie struct{ Movie }
@@ -25,7 +27,7 @@ func (x LVMovie) Search(search, page string) model.Pager {
 }
 
 func (x LVMovie) Detail(id string) model.MovieInfo {
-	return model.MovieInfo{}
+	return x.lvVideoDetail(id)
 }
 
 func (x LVMovie) Source(sid, vid string) model.Video {
@@ -79,4 +81,43 @@ func (x LVMovie) lvListBySearch(query, page string) model.Pager {
 	}
 
 	return pager
+}
+
+func (x LVMovie) lvVideoDetail(id string) model.MovieInfo {
+	var info = model.MovieInfo{}
+	info.Id = id
+
+	c := x.Movie.NewColly()
+
+	c.OnHTML(".stui-player__detail", func(element *colly.HTMLElement) {
+		info.Name = strings.TrimSpace(element.ChildText(".title"))
+		info.Intro = strings.TrimPrefix(element.ChildText(".desc"), "简介：")
+	})
+	c.OnResponse(func(response *colly.Response) {
+		// urls[1][0]='https://v10.dious.cc/20210707/jxnrMrb8/index.m3u8';
+		reg := regexp.MustCompile(`urls\[(\d+)\]\[(\d+)\]='(\S+)'`)
+		urlList := reg.FindAllStringSubmatch(string(response.Body), -1)
+		for _, urls := range urlList {
+			if len(urls) != 4 {
+				continue
+			}
+			info.Links = append(info.Links, model.Link{
+				Id:    fmt.Sprintf("%s-%s-%s", id, urls[1], urls[2]),
+				Name:  fmt.Sprintf("资源%s", urls[2]),
+				Url:   urls[3],
+				Group: fmt.Sprintf("group_%s", urls[1]),
+			})
+		}
+	})
+
+	c.OnRequest(func(request *colly.Request) {
+		log.Println("Visiting", request.URL.String())
+	})
+
+	err := c.Visit(fmt.Sprintf(lvDetailUrl, id))
+	if err != nil {
+		log.Println("[visit.error]", err.Error())
+	}
+
+	return info
 }
